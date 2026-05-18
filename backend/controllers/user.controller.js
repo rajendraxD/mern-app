983f1e559a4f3fac9b881d7af0fb84fc945996b0
rajendraxD <rajendraxd1@gmail.com>
2026-05-18 10:47:47 +0530

    feat: integrate axios for user data retrieval; update registration route and clean up commented code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,11 +3,16 @@ import {
   tryCatchHandler,
 } from "../middleware/errorHandler.middleware.js";
 import sanitize from "mongo-sanitize";
+import axios from "axios";
 export const register = tryCatchHandler(async (req, res) => {
-  const { username, email, password } = sanitize(req.body);
+  // const { username, email, password } = sanitize(req.body);
 
-  res.status(201).json({
-    status: "success",
-    message: "User registered successfully",
-  });
+  const user = axios.get(`https://jsonplaceholder.typicode.com/users`);
+  console.log(JSON.parse(JSON.stringify(user)));
+  console.log('------------------');
+  // res.status(201).json({
+  //   status: "success",
+  //   message: "User registered successfully",
+  //   data: JSON.parse(JSON.stringify(user)),
+  // });
 });
--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -5,45 +5,46 @@ import { body } from "express-validator";
 
 const router = express.Router();
 
-router.get(
-  "/register",
-  [
-    body("username")
-      .trim()
-      .notEmpty()
-      .withMessage("Username is required")
-      .isString()
-      .withMessage("Username must be a string")
-      .isAlpha("en-US", { ignore: " " })
-      .withMessage("Username must only contain letters")
-      .isLength({ min: 3 })
-      .withMessage("Username must be at least 3 characters long"),
-    body("email")
-      .trim()
-      .notEmpty()
-      .withMessage("Email is required")
-      .isEmail()
-      .withMessage("Please provide a valid email")
-    //   .custom(async (value) => {
-    //     const user = await UserModel.findOne({ email: value });
-    //     if (user) {
-    //       throw new Error("Email already in use");
-    //     }
-    //   }
-    // )
-    ,
-    body("password")
-      .trim()
-      .notEmpty()
-      .withMessage("Password is required")
-      .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters long")
-      .isLength({ max: 20 })
-      .withMessage("Password must be at most 20 characters long"),
-  ],
-  validate,
-  register,
-);
+// router.get(
+//   "/register",
+//   [
+//     body("username")
+//       .trim()
+//       .notEmpty()
+//       .withMessage("Username is required")
+//       .isString()
+//       .withMessage("Username must be a string")
+//       .isAlpha("en-US", { ignore: " " })
+//       .withMessage("Username must only contain letters")
+//       .isLength({ min: 3 })
+//       .withMessage("Username must be at least 3 characters long"),
+//     body("email")
+//       .trim()
+//       .notEmpty()
+//       .withMessage("Email is required")
+//       .isEmail()
+//       .withMessage("Please provide a valid email")
+//     //   .custom(async (value) => {
+//     //     const user = await UserModel.findOne({ email: value });
+//     //     if (user) {
+//     //       throw new Error("Email already in use");
+//     //     }
+//     //   }
+//     // )
+//     ,
+//     body("password")
+//       .trim()
+//       .notEmpty()
+//       .withMessage("Password is required")
+//       .isLength({ min: 6 })
+//       .withMessage("Password must be at least 6 characters long")
+//       .isLength({ max: 20 })
+//       .withMessage("Password must be at most 20 characters long"),
+//   ],
+//   validate,
+//   register,
+// );
+router.get("/register", register);
 // router.get("/login",login);
 // router.get("/logout",logout);
 
